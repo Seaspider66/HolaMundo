eb6b55672fb8b632d3f19f5c16ca7e08809f3192
jaimecursostenerife <jaimecursostenerife@gmail.com>
2025-05-13 11:26:03 +0100

    Primer commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        String saludar = "Hola mundo desde Java";
+        System.out.println(saludar);
+
+        //
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+        int numero = 11;
+        int numero1 = 20;
+        int total;
+
+        total = (numero1 + numero) - numero1;
+        System.out.println(total);
+    }
+
+
+}
