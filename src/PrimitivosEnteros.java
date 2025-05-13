7bb8cdb408853874d1e73e13eb7fb6a43c36b3f9
jaimecursostenerife <jaimecursostenerife@gmail.com>
2025-05-13 12:18:48 +0100

    Primer commit

---
+++ src/PrimitivosEnteros.java
@@ -0,0 +1,19 @@
+public class PrimitivosEnteros {
+    public static void main(String[] args) {
+        byte numeroByte = 127;
+        System.out.println("numero Byte" + numeroByte);
+
+        short numeroShort = 3267;
+        System.out.println("numero Short" + numeroShort);
+
+        int numeroInt = 2147483647;
+        System.out.println("numero Int" + numeroInt);
+
+        long numeroLong = 9223372036854775807L;
+
+        var numeroVar = 9223372036854775808f;
+
+        System.out.println(numeroVar);
+    }
+}
+
